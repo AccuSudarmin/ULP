b96add49c6b981ecdf9d3dfbdca4b15316767b04
aiiaccu <accusudarmin@gmail.com>
2015-08-26 14:49:47 +0800

    Update

--- application/views/head.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title> ULP </title>
-    <link href="<?php echo base_url('public/stylesheets/screen.css');?>" media="screen, projection" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url('public/stylesheets/print.css');?>" media="print" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url('public/stylesheets/ie.css');?>" media="screen, projection" rel="stylesheet" type="text/css" />
-  </head>
--- application/views/slider.php
+++ application/views/slider.php
@@ -1,40 +1,28 @@
-<div class="ninja-wrap">
-   <div class="slider-cont">
-         <div class="slide activeslide">
-            <img src="<?php echo base_url('public/image/a.jpg');?>">
+<div class='ninja-wrap'>
+   <div class='slider-cont'>
+         <div class='slide activeslide'>
+            <img src='image/a.jpg'>
          </div>
 
-         <div class="slide">
-            <img src="<?php echo base_url('public/image/b.png');?>">
+         <div class='slide'>
+            <img src='image/b.png'>
          </div>
 
-         <div class="slide">
-            <img src="<?php echo base_url('public/image/c.jpg');?>">
+         <div class='slide'>
+            <img src='image/c.jpg'>
          </div>
-
-         <?php foreach($slider->result() as $viewslider):?>
-            <div class="slide">
-               <img src="<?php echo base_url('public/image/'.$viewslider->slImg.'');?>">
-            </div>
-         <?php endforeach; ?>
-
    </div>
 
 </div>
 
 <div class="slider-nav">
-   <div class="slider-nav-wrapper">
-      <a href="#" class="arrow-prev"><img src="<?php echo base_url('public/image/arrow-prev.png');?>"></a>
+   <div class='slider-nav-wrapper'>
+      <a href="#" class="arrow-prev"><img src="image/arrow-prev.png"></a>
       <ul class="slider-dots">
         <li class="dot activedot">&bull;</li>
         <li class="dot">&bull;</li>
         <li class="dot">&bull;</li>
-        <?php
-           foreach($slider->result() as $mslider){
-             echo '<li class="dot">&bull;</li>';
-           }
-        ?>
       </ul>
-      <a href="#" class="arrow-next"><img src="<?php echo base_url('public/image/arrow-next.png');?>"></a>
+      <a href="#" class="arrow-next"><img src="image/arrow-next.png"></a>
    </div>
 </div>
