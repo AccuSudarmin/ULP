7c4aa10599bdc3846dc5020102ceafbe194ee6ab
Azrul <accusudarmin@gmail.com>
2015-08-26 14:59:51 +0800

    Update Accu

---
+++ application/views/administrator/header.php
@@ -0,0 +1,11 @@
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <link href="<?php echo base_url('public/css/style.css'); ?>" rel="stylesheet">
+      <link href="<?php echo base_url('public/css/graph_style.css'); ?>" rel="stylesheet">
+      <link href="<?php echo base_url('public/css/hiccup.css'); ?>" rel="stylesheet">
+      <script type="text/javascript" src="<?php echo base_url('public/js/function.js'); ?>"></script>
+      <meta name="viewport" content="width=450">
+      <title>Dashboard</title>
+   </head>
+   <body>
---
+++ application/views/administrator/navbar.php
@@ -0,0 +1,10 @@
+<nav id="navbar">
+   <div class="logo">
+      <span> DASHBOARD </span>
+   </div>
+   <div class="menu-admin right">
+      <div>
+         <a href="?ref=admin">Admin</a> | <a href="?ref=setting"> Setting </a> | <a href="?ref=logout"> Logout </a>
+      </div>
+   </div>
+</nav>
